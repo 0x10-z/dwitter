48f6891b020718663990ba36ff3693a8c5db05dc
iker ocio <ikerocio@yahoo.es>
2025-05-31 16:58:53 +0200

    Update base HTML structure and modify allowed hosts in settings

--- dwitter_project/settings.py
+++ dwitter_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0$#_=974%qkhh_=*oi!d)lr*p1g2ec+y-n+a5b)7*xsd@_6ycp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.1.139"]
 
 
 # Application definition
